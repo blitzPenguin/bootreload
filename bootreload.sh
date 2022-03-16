9e8d064f732945111d69bed9d3447c43e3989606
pikklz <joao.pedro.oliveira.pessoal@protonmail.com>
2022-03-16 14:37:14 +0000

    acabei o script do bootloader

--- bootreload.sh
+++ bootreload.sh
@@ -16,19 +16,20 @@
 
 #montar as partições
 
-mount /dev/nvme /mnt
-swapon /dev/nvme
+mount /dev/nvme0n1p6 /mnt
 mkdir /mnt/{home,boot}
 mount /dev/sda1 /mnt/boot
-mount /dev/nvme /mnt/home
+swapon /dev/nvme0n1p5
+mount /dev/nvme0n1p7 /mnt/home
 
 #reinstalar o bootloader
 
-grub-install --target=x86_64-efi --efi-directory=esp --bootloader-id=GRUB
+arch-chroot /mnt /root
+grub-install --target=x86_64-efi --efi-directory=/boot --recheck /dev/sda
 
 #desmontar as partições
 
-umount -all
+umount -R /mnt
 
 #reiniciar
 
