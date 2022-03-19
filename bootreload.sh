c6461ce643b57cd571f6165cde6669fe2013c4c7
pikklz <joao.pedro.oliveira.pessoal@protonmail.com>
2022-03-19 18:42:57 +0000

    finalização do script

--- bootreload.sh
+++ bootreload.sh
@@ -22,14 +22,21 @@ mount /dev/sda1 /mnt/boot
 swapon /dev/nvme0n1p5
 mount /dev/nvme0n1p7 /mnt/home
 
+read -p "Particoes montadas..."
+
+
 #reinstalar o bootloader
 
 arch-chroot /mnt /root
 grub-install --target=x86_64-efi --efi-directory=/boot --recheck /dev/sda
 
+read -p "Grub reinstalado..."
+
 #desmontar as partições
 
 umount -R /mnt
+
+read -p "Particoes desmontadas, press enter pa reiniciar..."
 
 #reiniciar
 
