699bdc6fab705b63d6b33ef401ae48421d80ff73
pikklz <joao.pedro.oliveira.pessoal@protonmail.com>
2022-03-31 17:13:07 +0100

    new changes to script

--- bootreload.sh
+++ bootreload.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
-#======================================================================
-
- ####   ####  ##  #  ##  #  ##     ####
- ##  #   ##   ## #   ## #   ##       #
- ####    ##   ###    ###    ##      # 
- ##      ##   ## #   ## #   ##     #  
- ##     ####  ##  #  ##  #  ####  ####
-
-#======================================================================
-
-#=======================================================================
-#Script para reinstalar bootloader na pen para o pc do curso
-#=======================================================================
+echo " #======================================================================"
+echo ""
+echo "  ####   ####  ##  #  ##  #  ##     ####"
+echo "  ##  #   ##   ## #   ## #   ##       #"
+echo "  ####    ##   ###    ###    ##      #"
+echo "  ##      ##   ## #   ## #   ##     #"
+echo "  ##     ####  ##  #  ##  #  ####  ####"
+echo ""
+echo " #======================================================================"
+echo ""
+echo " #======================================================================"
+echo " #Script para reinstalar bootloader na pen para o pc do curso"
+echo " #======================================================================"
+echo ""
 
 #montar as partições
 
 mount /dev/nvme0n1p6 /mnt
-mkdir /mnt/{home,boot}
 mount /dev/sda1 /mnt/boot
 swapon /dev/nvme0n1p5
 mount /dev/nvme0n1p7 /mnt/home
@@ -27,7 +27,7 @@ read -p "Particoes montadas..."
 
 #reinstalar o bootloader
 
-arch-chroot /mnt /root
+arch-chroot /mnt
 grub-install --target=x86_64-efi --efi-directory=/boot --recheck /dev/sda
 
 read -p "Grub reinstalado..."
